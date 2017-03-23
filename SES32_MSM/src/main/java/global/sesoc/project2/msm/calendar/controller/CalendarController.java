d34e568f02579fe0bd760c13cc43f3f6524496f3
kita <kita@kita-PC>
2017-03-24 08:35:39 +0900

    에러방지 커밋 (제이슨관련 임포트문 제거) 170324 재민

--- SES32_MSM/src/main/java/global/sesoc/project2/msm/calendar/controller/CalendarController.java
+++ SES32_MSM/src/main/java/global/sesoc/project2/msm/calendar/controller/CalendarController.java
@@ -2,7 +2,6 @@ package global.sesoc.project2.msm.calendar.controller;
 
 import java.util.ArrayList;
 
-import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
