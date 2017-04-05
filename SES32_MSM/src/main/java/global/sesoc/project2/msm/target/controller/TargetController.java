6d7cc41bfc8ce73f4fdd9e151ad10c17f113e948
kita <kita@kita-PC>
2017-04-05 11:16:54 +0900

    1. 샘플엑셀 다운로드 수정   - config파일에 항목 추가   - jsp경로 수정 2. DB데이터 엑셀저장기능 추가   - 추후 확장성을 고려하여 DataVO추가 후 accBookVO만 extends 처리   - pom에 jxl관련 항목 추가   - Controller, DAO, mapper수정
    170405 재민

--- SES32_MSM/src/main/java/global/sesoc/project2/msm/target/controller/TargetController.java
+++ SES32_MSM/src/main/java/global/sesoc/project2/msm/target/controller/TargetController.java
@@ -1,17 +1,31 @@
 package global.sesoc.project2.msm.target.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 import global.sesoc.project2.msm.target.dao.TargetDAO;
 import global.sesoc.project2.msm.target.vo.TargetVO;
+import global.sesoc.project2.msm.util.ExcelService;
 import global.sesoc.project2.msm.util.FileService;
 
 /**
@@ -23,55 +37,149 @@ public class TargetController {
 	Logger log = LoggerFactory.getLogger(TargetController.class);
 	
 	// config.properties사용하여 값 가져오기
-		@Value("#{config['EXCEL_UPLOAD_PATH']}")
-		String uploadPath; // 엑셀 업로드 기능 동작시 임시경로
-		
-		@Value("#{config['EXCEL_DOWNLOAD_PATH']}")
-		String downloadPath; // 엑셀 다운로드 기능 동작시 임시경로
-		
-		@Autowired
-		TargetDAO dao;
-		
-		@RequestMapping("excelTest")
-		public String excelServicePage() {
-			log.debug("excelServicePage - test page이동");
-			return "target/excelService"; 
-		}
+	@Value("#{config['EXCEL_UPLOAD_PATH']}")
+	String uploadPath; // 엑셀 업로드 기능 동작시 임시경로
+	
+	@Value("#{config['EXCEL_DOWNLOAD_PATH']}")
+	String downloadPath; // 엑셀 다운로드 기능 동작시 임시경로
+	
+	@Value("#{config['SAMPLE_EXCEL']}")
+	String samplePath; // 엑셀업로드 샘플파일 경로
+	
+	@Autowired
+	TargetDAO dao;
+	
+	@RequestMapping("excelTest")
+	public String excelServicePage() {
+		log.debug("excelServicePage - test page이동");
+		return "target/excelService"; 
+	}
 
-		@RequestMapping(value="excelUpload", method=RequestMethod.POST)
-		public String excelUpload(
-				MultipartFile upload
-				, Model model) {
-			
-			log.debug("excelUpload :: POST");
-			
-			log.debug("contentType: {}", upload.getContentType());
-			log.debug("name: {}", upload.getName());
-			log.debug("original name: {}", upload.getOriginalFilename());
-			log.debug("size: {}", upload.getSize());
-			
-			String ori_file = upload.getOriginalFilename();
-			String extension = ori_file.substring(ori_file.lastIndexOf(".")+1, ori_file.length());
-			log.debug("extension : {}", extension);
-			if(extension.contains("xls")) { // 유저가 업로드한 파일이 엑셀일때
-				if(!upload.isEmpty()) {
-					// 특정 폴더에 엑셀파일 업로드
-					String file_name = FileService.saveFile(upload, uploadPath);
-					
-					// 업로드 완료후 return
-					int ret = dao.excelUpload(uploadPath + "/" + file_name);
-					if(ret > 0) {
-						// 업로드 및 DB insert가 완료되면 파일을 삭제한다.
-						FileService.deleteFile(uploadPath + "/" + file_name);
-					}
+	@RequestMapping(value="excelUpload", method=RequestMethod.POST)
+	public String excelUpload(
+			MultipartFile upload
+			, Model model) {
+		
+		log.debug("excelUpload :: POST");
+		
+		log.debug("contentType: {}", upload.getContentType());
+		log.debug("name: {}", upload.getName());
+		log.debug("original name: {}", upload.getOriginalFilename());
+		log.debug("size: {}", upload.getSize());
+		
+		String ori_file = upload.getOriginalFilename();
+		String extension = ori_file.substring(ori_file.lastIndexOf(".")+1, ori_file.length());
+		log.debug("extension : {}", extension);
+		if(extension.contains("xls")) { // 유저가 업로드한 파일이 엑셀일때
+			if(!upload.isEmpty()) {
+				// 특정 폴더에 엑셀파일 업로드
+				String file_name = FileService.saveFile(upload, uploadPath);
+				
+				// 업로드 완료후 return
+				String up_path = uploadPath + "/" + file_name;
+				log.debug("{}", up_path);
+				int ret = dao.excelUpload(up_path);
+				if(ret > 0) {
+					// 업로드 및 DB insert가 완료되면 파일을 삭제한다.
+					boolean del_ret = FileService.deleteFile(up_path);
+					log.debug("del?! {}", del_ret);
+					log.debug("-----------------엑셀업로드 기능 끝-----------------");
 				}
-				model.addAttribute("up_ret", "ok");
-			} else { // 유저가 업로드한 파일이 엑셀이 아닌 다른 파일일때
-				model.addAttribute("up_ret", "only excel file!!!");
+			}
+			model.addAttribute("up_ret", "ok");
+		} else { // 유저가 업로드한 파일이 엑셀이 아닌 다른 파일일때
+			model.addAttribute("up_ret", "only excel file!!!");
+		}
+		return "target/excelService";
+	}
+	
+	@RequestMapping("sampleDown")
+	public void sampleExcelDownload(
+			HttpServletResponse resp
+			, HttpServletRequest req) {
+		try {
+			resp.setHeader("Content-Disposition", 
+						"attachment;filename=" 
+						+ URLEncoder.encode("업로드샘플.xlsx", "utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		log.debug(req.getSession().getServletContext().getRealPath(samplePath)); // 서버메인 root_path
+		
+		String down_path = req.getSession().getServletContext().getRealPath(samplePath);
+		
+		FileInputStream in = null;
+		ServletOutputStream out = null;
+		try {
+			in = new FileInputStream(down_path);
+			out = resp.getOutputStream();
+			
+			FileCopyUtils.copy(in, out);
+		} catch (FileNotFoundException e) {
+			log.error(e.getMessage());
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		} finally {
+			try {
+				if(in != null) in.close();
+				if(out != null) out.close();
+			} catch (IOException e) {
+				log.error(e.getMessage());
+			}
+		}
+	}
+	
+	@RequestMapping("excelDown")
+	public void downloadDataToExcel(HttpServletResponse resp) {
+		log.debug("downloadDataToExcel");
+		try {
+			resp.setHeader("Content-Disposition", 
+						"attachment;filename=" 
+						+ URLEncoder.encode("경조사가계부내역.xls", "utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		HashMap<String, Object> param = new HashMap<>();
+		param.put("group_name", "보리");
+		
+		String[] members = {"ta_date", "ta_memo", "t_group", "t_name", "ta_price"};
+		String[] cell_headers = {"일자", "경조사", "소속", "이름", "금액"};
+		int[] cell_widths = {20, 30, 30, 20, 10};
+		
+		try {
+			//저장 폴더가 없으면 생성
+			File path = new File(downloadPath);
+			if (!path.isDirectory()) {
+				path.mkdirs();
 			}
 			
+			String save_path = downloadPath + "/test.xls";
 			
+			ExcelService.simpleExcelWrite(new File(save_path)
+					, dao.selectTargetAccBook(param), members, cell_headers, cell_widths);
 			
-			return "target/excelService";
+			FileInputStream in = null;
+			ServletOutputStream out = null;
+			try {
+				in = new FileInputStream(save_path);
+				out = resp.getOutputStream();
+				
+				FileCopyUtils.copy(in, out);
+			} catch (FileNotFoundException e) {
+				log.error(e.getMessage());
+			} catch (IOException e) {
+				log.error(e.getMessage());
+			} finally {
+				try {
+					if(in != null) in.close();
+					if(out != null) out.close();
+				} catch (IOException e) {
+					log.error(e.getMessage());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
 }
--- SES32_MSM/src/main/java/global/sesoc/project2/msm/target/dao/TargetDAO.java
+++ SES32_MSM/src/main/java/global/sesoc/project2/msm/target/dao/TargetDAO.java
@@ -1,5 +1,7 @@
 package global.sesoc.project2.msm.target.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import global.sesoc.project2.msm.target.mapper.ITargetMapper;
 import global.sesoc.project2.msm.target.vo.TargetAccBookVO;
 import global.sesoc.project2.msm.target.vo.TargetVO;
+import global.sesoc.project2.msm.util.DataVO;
 import global.sesoc.project2.msm.util.ExcelService;
 
 /**
@@ -23,46 +26,68 @@ public class TargetDAO {
 	@Autowired
 	SqlSession sqlSession;
 	
+	/**
+	 * 엑셀 업로드
+	 * @param file_name 저장될 파일 경로
+	 * @return
+	 */
 	public int excelUpload(String file_name) {
 		log.debug("excelUpload..... : filename? {}", file_name);
 		
 		int ret = 0;
 		ITargetMapper mapper = sqlSession.getMapper(ITargetMapper.class);
-		List<List<String>> data = ExcelService.getExcelList(file_name);
-		
-		// DB insert
-		for (List<String> list : data) {
-			// 경조사 :: 입력형식 : yyyy-mm-dd 경조사명
-			String event = list.get(0);
-			// 경조사 일자
-			String event_date = event.substring(0, event.lastIndexOf("-") + 3);
-			// 경조사관련 메모
-			String event_memo = event.substring(event.lastIndexOf("-") + 4, event.length());
+		List<List<String>> data;
+		try {
+			data = ExcelService.getExcelList(file_name);
 			
-			// 1. 대상자 등록
-			TargetVO tVO = new TargetVO();
-			tVO.setT_name(list.get(1));
-			tVO.setT_group(list.get(3));
-			tVO.setT_date(event_date);
-			log.debug("before insert target : {}", tVO);
-			ret = mapper.insertTarget(tVO);
-			
-			if(ret > 0) {
-				// 대상자관련 가계부에 등록을 위한 대상자 아이디 얻기
-				int t_id = mapper.selectLatestTarget();
+			// DB insert
+			for (List<String> list : data) {
+				// 경조사 :: 입력형식 : yyyy-mm-dd 경조사명
+				String event = list.get(0);
+				// 경조사 일자
+				String event_date = event.substring(0, event.lastIndexOf("-") + 3);
+				// 경조사관련 메모
+				String event_memo = event.substring(event.lastIndexOf("-") + 4, event.length());
 				
-				// 2. 대상자관련 가계부 등록
-				TargetAccBookVO tAccVO = new TargetAccBookVO();
-				tAccVO.setT_id(t_id);
-				tAccVO.setTa_price(Integer.parseInt(list.get(2)));
-				tAccVO.setTa_type("IN");
-				tAccVO.setTa_memo(event_memo);
-				tAccVO.setTa_date(event_date);
-				log.debug("before insert target accbook : {}", tAccVO);
-				ret = mapper.insertTargetAccbook(tAccVO);
+				// 1. 대상자 등록
+				TargetVO tVO = new TargetVO();
+				tVO.setT_name(list.get(1));
+				tVO.setT_group(list.get(3));
+				tVO.setT_date(event_date);
+				log.debug("before insert target : {}", tVO);
+				ret = mapper.insertTarget(tVO);
+				
+				if(ret > 0) {
+					// 대상자관련 가계부에 등록을 위한 대상자 아이디 얻기
+					int t_id = mapper.selectLatestTarget();
+					
+					// 2. 대상자관련 가계부 등록
+					TargetAccBookVO tAccVO = new TargetAccBookVO();
+					tAccVO.setT_id(t_id);
+					tAccVO.setTa_price(Integer.parseInt(list.get(2)));
+					tAccVO.setTa_type("IN");
+					tAccVO.setTa_memo(event_memo);
+					tAccVO.setTa_date(event_date);
+					log.debug("before insert target accbook : {}", tAccVO);
+					ret = mapper.insertTargetAccbook(tAccVO);
+				}
 			}
+			sqlSession.commit();
+		} catch (Exception e) {
+			sqlSession.rollback();
+			log.error(e.getMessage());
 		}
 		
 		return ret;
+	}
+	
+	/**
+	 * 가계부 내역 얻기
+	 * @param param 검색어 hashmap
+	 * @return
+	 */
+	public ArrayList<DataVO> selectTargetAccBook(HashMap<String, Object> param) {
+		ITargetMapper mapper = sqlSession.getMapper(ITargetMapper.class);
+		return mapper.selectTargetAccBook(param);
 	}
 }
