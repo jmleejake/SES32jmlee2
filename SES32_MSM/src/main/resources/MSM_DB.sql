c487b57f7df80c7a0e1aed936905f3cbdbe7aa4a
kita <kita@kita-PC>
2017-04-29 18:00:26 +0900

    sql 수정 2017-04-29 장진석

--- SES32_MSM/src/main/resources/MSM_DB.sql
+++ SES32_MSM/src/main/resources/MSM_DB.sql
@@ -1,10 +1,10 @@
 
 /* Drop Tables */
 
+DROP TABLE MSM_TARGET_ACC_BOOK CASCADE CONSTRAINTS;
 DROP TABLE MSM_ACC_BOOK CASCADE CONSTRAINTS;
 DROP TABLE MSM_CALENDAR CASCADE CONSTRAINTS;
 DROP TABLE MSM_TARGET CASCADE CONSTRAINTS;
-DROP TABLE MSM_TARGET_ACC_BOOK CASCADE CONSTRAINTS;
 DROP TABLE MSM_USER CASCADE CONSTRAINTS;
 
 /* Drop Sequences */
@@ -23,10 +23,24 @@ CREATE SEQUENCE SEQ_MSM_TARGET_ACC;
 
 /* Create Tables */
 
+CREATE TABLE MSM_USER
+(
+	u_id varchar2(20) NOT NULL,
+	u_pwd varchar2(20) NOT NULL,
+	u_name varchar2(20) NOT NULL,
+	u_email varchar2(40) NOT NULL,
+	u_phone varchar2(30),
+	u_birth varchar2(50),
+	u_address varchar2(70),
+	u_emergences number DEFAULT 0, -- ** 회원가입 시 초기 설정 및 이후 누적 잔여액수
+	PRIMARY KEY (u_id)
+);
+
+
 CREATE TABLE MSM_ACC_BOOK
 (
 	a_id number NOT NULL,
-	u_id varchar2(20) NOT NULL,
+	u_id varchar2(20) references MSM_USER on delete cascade ,
 	a_date date,
 	a_type varchar2(20) , -- **수입: IN / 지출: OUT / 비상: BIS
 	main_cate varchar2(20) ,
@@ -41,7 +55,7 @@ CREATE TABLE MSM_ACC_BOOK
 CREATE TABLE MSM_CALENDAR
 (
 	id number NOT NULL,
-	u_id varchar2(20) NOT NULL,
+	u_id varchar2(20)  references MSM_USER on delete cascade ,
 	t_id number,
 	text varchar2(100) NOT NULL,
 	start_date date NOT NULL,
@@ -62,7 +76,7 @@ CREATE TABLE MSM_CALENDAR
 CREATE TABLE MSM_TARGET
 (
 	t_id number NOT NULL,
-	u_id varchar2(20) NOT NULL,
+	u_id varchar2(20)  references MSM_USER on delete cascade ,
 	t_name varchar2(40) NOT NULL,
 	t_date date NOT NULL,
 	t_birth varchar2(20),
@@ -74,27 +88,16 @@ CREATE TABLE MSM_TARGET
 CREATE TABLE MSM_TARGET_ACC_BOOK
 (
 	ta_id number NOT NULL,
+	t_id number  references MSM_TARGET on delete cascade ,
 	ta_date date,
 	ta_price number,
 	ta_type char(3),
 	ta_memo varchar2(300),
-	t_id number,
 	PRIMARY KEY (ta_id)
 );
 
 
-CREATE TABLE MSM_USER
-(
-	u_id varchar2(20) NOT NULL,
-	u_pwd varchar2(20) NOT NULL,
-	u_name varchar2(20) NOT NULL,
-	u_email varchar2(40) NOT NULL,
-	u_phone varchar2(30),
-	u_birth varchar2(50),
-	u_address varchar2(70),
-	u_emergences number DEFAULT 0, -- ** 회원가입 시 초기 설정 및 이후 누적 잔여액수
-	PRIMARY KEY (u_id)
-);
+delete MSM_USER where u_id = 'aaa';
 
 /* 테스트 계정 */
 insert into MSM_USER values ('aaa','aaa','김태희','adolftaehee2016@gmail.com','010-1111-1111','1990-10-21','오사카부 교토시', 0);
