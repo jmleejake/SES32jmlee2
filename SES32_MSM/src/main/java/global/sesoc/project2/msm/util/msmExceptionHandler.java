ed57560118a7dcc4b73524592d7a7f62e706c2f6
kita <kita@kita-PC>
2017-04-30 16:11:34 +0900

    필요 없는 코드 삭제 및 인터셉터 에러 페이지 주석해제

--- SES32_MSM/src/main/java/global/sesoc/project2/msm/util/msmExceptionHandler.java
+++ SES32_MSM/src/main/java/global/sesoc/project2/msm/util/msmExceptionHandler.java
@@ -1,13 +1,14 @@
 package global.sesoc.project2.msm.util;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class msmExceptionHandler {
 
-	/*@Ex(Exception.class)
+	@ExceptionHandler(Exception.class)
 	public String errorHandler() {
 
 		return "/error"; // views 폴더의 error.jsp로 포워딩
-	}*/
+	}
 }
--- SES32_MSM/src/main/webapp/WEB-INF/views/error.jsp
+++ SES32_MSM/src/main/webapp/WEB-INF/views/error.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>에러</h1>
+
 </body>
 </html>
