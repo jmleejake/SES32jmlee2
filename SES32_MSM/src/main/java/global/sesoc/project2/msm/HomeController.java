2843f190c90abdd260461c138f632eba9842ce6f
kita <kita@kita-PC>
2017-07-14 16:42:12 +0900

    안드로이드 테스트

--- SES32_MSM/src/main/java/global/sesoc/project2/msm/HomeController.java
+++ SES32_MSM/src/main/java/global/sesoc/project2/msm/HomeController.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import global.sesoc.project2.msm.user.dao.UserDAO;
 
@@ -35,5 +37,18 @@ public class HomeController {
 	public String newhome2() {
 		logger.debug("go to login new home!");
 		return "newhome2";
+	}
+	
+	/**
+	 * @param text 안드로이드에서 넘어온 데이터
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="android", method=RequestMethod.POST)
+	public String methodForAndroid(@RequestBody String text) {
+		logger.debug("전송받은 문자열 : {}", text);
+		String jsonText = "{'userlist':[{'num' : '01' , 'name' : 'leejongho' , 'phone' : '010-1234-1234'},"
+				+ "{'num' : '02' , 'name' : 'leejongho2' , 'phone' : '010-1234-1235'}]}";
+		return jsonText;
 	}
 }
