d95940720162ad38a774b06a70d667bc5e8a5435
kita <kita@kita-PC>
2017-07-25 09:49:21 +0900

    안드로이드 프로젝트 작업

--- SES32_MSM/src/main/java/global/sesoc/project2/msm/HomeController.java
+++ SES32_MSM/src/main/java/global/sesoc/project2/msm/HomeController.java
@@ -1,5 +1,8 @@
 package global.sesoc.project2.msm;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import global.sesoc.project2.msm.user.dao.UserDAO;
+import global.sesoc.project2.msm.user.vo.UserVO;
 
 @Controller
 public class HomeController {
@@ -40,15 +44,78 @@ public class HomeController {
 	}
 	
 	/**
-	 * @param text 안드로이드에서 넘어온 데이터
+	 *  안드로이드 로그인
+	 * @param data 안드로이드에서 넘어온 로그인 데이터
+	 * @return
+	 * @throws JSONException
+	 */
+	@ResponseBody
+	@RequestMapping(value = "android_login", method = RequestMethod.POST)
+	public String methodForAndroidLogin(@RequestBody String data) throws JSONException {
+
+		logger.debug("user : " + data);
+
+		String jsonText = null;
+
+		JSONArray array = new JSONArray(data);
+
+		String id = null;
+		String pw = null;
+
+		for (int i = 0; i < array.length(); i++) {
+			JSONObject json = array.getJSONObject(i);
+			
+			id = json.get("userid").toString();
+			pw = json.get("userpw").toString();
+			
+			UserVO vo = dao.userLogin(id, pw);
+			//로그인 실패
+			if(vo==null){
+				jsonText = "아이디나 비밀번호가 틀립니다.";
+			} else {
+				JSONObject obj = new JSONObject();
+				obj.put("user_id", vo.getU_id());
+				obj.put("user_name", vo.getU_name());
+				JSONArray arr = new JSONArray();
+				arr.put(obj);
+				jsonText = arr.toString();
+			}
+		}
+
+		return jsonText;
+	}
+	
+	/**
+	 * 안드로이드에서 넘어온 카드데이터 처리
+	 * @param data
 	 * @return
 	 */
 	@ResponseBody
-	@RequestMapping(value="android", method=RequestMethod.POST)
-	public String methodForAndroid(@RequestBody String text) {
-		logger.debug("전송받은 문자열 : {}", text);
+	@RequestMapping(value="android_data", method=RequestMethod.POST)
+	public String methodForAndroidDataProcess(@RequestBody String data) {
+		logger.debug("전송받은 문자열 : {}", data);
 		String jsonText = "{'userlist':[{'num' : '01' , 'name' : 'leejongho' , 'phone' : '010-1234-1234'},"
 				+ "{'num' : '02' , 'name' : 'leejongho2' , 'phone' : '010-1234-1235'}]}";
+		JSONArray array;
+		try {
+			array = new JSONArray(data);
+			
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject obj = array.getJSONObject(i);
+				logger.debug(obj.getString("tel"));
+				logger.debug(obj.getString("date"));
+				logger.debug(obj.getString("content"));
+				
+				String telNo = obj.getString("tel");
+				String date = obj.getString("date");
+				String content = obj.getString("content");
+				
+			}
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
 		return jsonText;
 	}
 }
